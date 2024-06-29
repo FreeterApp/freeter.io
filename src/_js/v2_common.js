7002d73376e48be62f8e13d88ce68fe899cf20d2
alexk111 <gh@alexkaul.com>
2024-06-29 18:28:40 +0400

    Add tooltip to Donate button

--- src/_js/v2_common.js
+++ src/_js/v2_common.js
@@ -1,1 +1,12 @@
 require('bootstrap/js/dist/collapse');
+
+$(function () {
+  setTimeout(function () {
+    $.get( "/sponsors.json", function( sponsors ) {
+      if(sponsors.length>0) {
+        var sponsor = sponsors[Math.floor(Math.random() * sponsors.length)]
+        $('.sponsor-tooltip').text('Thanks to ' + sponsor + ' & others...').removeClass('is-loading');
+      }
+    });
+  }, 100)
+})
