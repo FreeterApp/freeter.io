2bbd0b131069576800e7f1506177cc372ff05c29
alexk111 <gh@alexkaul.com>
2024-06-29 18:40:20 +0400

    Move static json to /data

--- src/_js/v2_common.js
+++ src/_js/v2_common.js
@@ -2,7 +2,7 @@ require('bootstrap/js/dist/collapse');
 
 $(function () {
   setTimeout(function () {
-    $.get( "/sponsors.json", function( sponsors ) {
+    $.get( "/data/sponsors.json", function( sponsors ) {
       if(sponsors.length>0) {
         var sponsor = sponsors[Math.floor(Math.random() * sponsors.length)]
         $('.sponsor-tooltip').text('Thanks to ' + sponsor + ' & others...').removeClass('is-loading');
