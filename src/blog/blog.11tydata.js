0210e32e13e185d87689faa87f13a85429c0cd47
alexk111 <gh@alexkaul.com>
2025-09-05 13:35:02 +0400

    Upd

--- src/blog/blog.11tydata.js
+++ src/blog/blog.11tydata.js
@@ -2,5 +2,8 @@ module.exports = {
   layout: 'v2_blog-page.njk',
   eleventyComputed: {
     summary_25: data => data.summary && data.summary.split(' ').slice(0, 25).join(' '),
+    pageImage: data => data.summary /* is blog post*/ ? `${ data.page.url }index.png` : data.pageImage,
+    pageTitle: data => data.title || data.pageTitle,
+    pageDescription: data => data.summary ? data.summary.split(' ').slice(0, 25).join(' ') : data.pageDescription,
   }
 }
